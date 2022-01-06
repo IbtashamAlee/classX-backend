8610fc629d3797eefe8274132025fdb501e1bea8
ibtasham <aleejaan562@gmail.com>
2022-01-06 18:13:28 +0500

    adds test routes to test postgres

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+var { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+router.get('/', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+router.post(`/`, async (req, res) => {
+  const result = await prisma.user.create({
+    data: { ...req.body },
+  })
+  res.json(result)
+})
+
+router.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: false,
+      author: { connect: { email: authorEmail } },
+    },
+  })
+  res.json(result)
+})
+
 
 module.exports = router;
