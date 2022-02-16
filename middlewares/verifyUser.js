768c73ae9b225e94b2ef95ad68fb26cd4ed5680a
faseehahmad00 <faseehahmad00@gmail.com>
2022-02-16 20:23:18 +0500

    added endpoint to get current user

---
+++ middlewares/verifyUser.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function verifyUser(req, res, next) {
+    try {
+    let token = req.header("auth-token");
+    if (!token)
+        return res.status(400).send("Token not provided");
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(user);
+        if (!user)
+            return res.status(403).send("invalid token");
+        const dbUser = await prisma.user.findUnique({
+            where: {
+                id: user.id
+            }
+        })
+        req.user = dbUser;
+        next();
+    } catch (e) {
+        return res.status(401).send("not authorized");
+    }
+
+}
+
+module.exports.verifyUser = verifyUser;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {verifySystemAdmin} = require('../middlewares/verifySystemAdmin');
+const {verifyUser} = require('../middlewares/verifyUser');
 const{ PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
@@ -9,5 +10,10 @@ router.get('/',verifySystemAdmin, async (req, res) => {
   res.json(users)
 })
 
+//write a endpoint to get current user
+router.get('/me',verifyUser, async (req, res) => {
+
+  res.send(req.user)
+})
 
 module.exports = router;
