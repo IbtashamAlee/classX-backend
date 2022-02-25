51f451a7d34a4698cb4db7b7f570d08c4585bd1a
faseehahmad00 <faseehahmad00@gmail.com>
2022-02-25 19:36:36 +0500

    fixed authentication functions

--- routes/authentication.js
+++ routes/authentication.js
@@ -57,27 +57,23 @@ router.post(`/signup`, signupValidation, async (req, res) => {
 
 //This route processes user request for email verification
 router.get("/verify-mail/:id", async (req, res) => {
-    const {id} = req.params;
     try {
-        const temp = id.split("=");
+        const temp = req.params.id.split("=");
         const token = temp[0];
         const mail = temp[1];
-        console.log("mail is " + mail);
         const result = await prisma.user.findUnique({
             where: {
                 email: mail,
             },
         });
-        if (token === result.emailToken) {
-            await prisma.user.update({
-                where: {
-                    email: mail,
-                }, data: {
-                    isVerified: true,
-                },
-            });
-        }
-        return res.status(200).send("Email Verified Successfully. Continue to login");
+        if (token !== result.emailToken) return res.status(400).send("invalid verification token.")
+        await prisma.user.update({
+            where: {
+                email: mail,
+            }, data: {
+                isVerified: true,
+            },
+        }).then(() => res.status(200).send("Email Verified Successfully. Continue to login"))
     } catch (e) {
         return res.status(403).send("Unable to Verify .Please Try Again");
     }
@@ -86,52 +82,35 @@ router.get("/verify-mail/:id", async (req, res) => {
 //This is route for user login
 router.post(`/login`, loginValidation, async (req, res) => {
     try {
-        await prisma.user.findUnique({
+        const user = await prisma.user.findUnique({
             where: {
                 email: req.body.email,
             },
-        }).then((user) => {
-            if (user) {
-                verifyPassword(req.body.password, user.password)
-                    .then((result) => {
-                        if (result) {
-                            if (user.isVerified) {
-                                //IF user exists , password matches and user is verified then proceed to login
-                                //parsing user agent header for digital finger print
-                                const myParser = new parser();
-                                myParser.setUA(req.headers["user-agent"]);
-                                const result = myParser.getResult();
-                                //generating random session token
-                                const sessionToken = randomstring.generate(240);
-                                //generating user session
-                                prisma.userSession.create({
-                                    data: {
-                                        userId: user.id,
-                                        createdAt: new Date(),
-                                        userAgent: result.ua || "unknown",
-                                        ipv4Address: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
-                                        ipv6Address: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
-                                        device_model: result.device.type + "-" + result.device.vendor + "-" + result.device.model || "unknown",
-                                        browser_version: result.browser.version || "unknown",
-                                        browser_family: result.browser.name || "unknown",
-                                        os_family: result.os.name || "unknown",
-                                        os_version: result.os.version || "unknown",
-                                        token: sessionToken + user.id,
-                                    }
-                                }).then(({token}) => {
-                                    return res.status(200).send({access_token: token});
-                                })
-                            } else {
-                                return res.status(401).send("USER MAIL IS NOT VERIFIED . PLEASE VERIFY YOUR EMAIL TO LOGIN");
-                            }
-                        } else {
-                            return res.status(403).send("Password is incorrect");
-                        }
-                    });
-            } else {
-                return res.status(404).send("user not found");
+        })
+        if (!user) return res.status(404).send("User not found");
+        const validPassword = await verifyPassword(req.body.password, user.password)
+        if (!validPassword) return res.status(403).send("Password is incorrect");
+        if (!user.isVerified) return res.status(401).send("Please verify your email first");
+        const myParser = new parser();
+        myParser.setUA(req.headers["user-agent"]);
+        const result = myParser.getResult();
+        const sessionToken = randomstring.generate(240);
+        const session = await prisma.userSession.create({
+            data: {
+                userId: user.id,
+                createdAt: new Date(),
+                userAgent: result.ua || "unknown",
+                ipv4Address: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+                ipv6Address: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+                device_model: result.device.type + "-" + result.device.vendor + "-" + result.device.model || "unknown",
+                browser_version: result.browser.version || "unknown",
+                browser_family: result.browser.name || "unknown",
+                os_family: result.os.name || "unknown",
+                os_version: result.os.version || "unknown",
+                token: sessionToken + user.id,
             }
-        });
+        })
+        return res.status(200).send({access_token: session.token});
     } catch (e) {
         res.send(e);
     }
@@ -140,50 +119,42 @@ router.post(`/login`, loginValidation, async (req, res) => {
 // manually resend verification to registered user
 router.post("/sendemailverification", async (req, res) => {
     const mail = req.body.email;
-    const result = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
         where: {
             email: mail,
         },
     });
-    console.log(result);
-    if (result) {
-        verifyPassword(req.body.password, result.password)
-            .then((status) => {
-                if (status) {
-                    if (result.isVerified) return res.send("User is already Verified"); else {
-                        sendVerification(result.name, result.email, result.emailToken)
-                            .then(() => {
-                                return res.status(200).send("Verification Email sent");
-                            });
-                    }
-                } else return res.status(401).send("Not Authorized");
-            })
-    } else return res.status(404).send("User not Found");
+    if (!user) return res.status(404).send("User not Found");
+    const validPassword = await verifyPassword(req.body.password, result.password)
+    if (!validPassword) return res.status(401).send("Not Authorized");
+    if (user.isVerified) return res.send("User is already Verified");
+    sendVerification(user.name, user.email, user.emailToken)
+        .then(() => res.status(200).send("Verification Email sent"));
 });
 
-router.post('/makeAdmin', verifySystemAdmin, async (req, res) => {
+//System admin can make another user system admin
+router.post('/makeAdmin', verifyUser, verifySystemAdmin, async (req, res) => {
     try {
-        if (req.body.email) {
-            const user = await prisma.user.findUnique({
-                where: {
-                    email: req.body.email,
-                }
-            })
-            if (user) {
-                const role = await prisma.role.findMany({
-                    where: {
-                        name: "SystemAdmin"
-                    }
-                });
-                const userRole = await prisma.userRole.create({
-                    data: {
-                        userId: user.id, roleId: role[0].id
-                    }
-                })
-                res.status(200).send({resp: "admin privileges created for user", userRole});
-            } else res.status(404).send("User not found");
-        } else return res.status(401).send("email not provided");
-    } catch (e) {
+        if(!req.body.email) return res.status(401).send("email not provided");
+        const user = await prisma.user.findUnique({
+            where: {
+                email: req.body.email,
+            }
+        })
+        if(!user) return res.status(404).send("User not found");
+        const role = await prisma.role.findMany({
+            where: {
+                name: "SystemAdmin"
+            }
+        });
+        const userRole = await prisma.userRole.create({
+            data: {
+                userId: user.id, roleId: role[0].id
+            }
+        })
+        return res.status(200).send({resp: "admin privileges created for user", userRole});
+    }
+    catch (e) {
         if (e.code === 'P2002') {
             return res.status(409).send("Role Already Exists");
         }
@@ -191,23 +162,22 @@ router.post('/makeAdmin', verifySystemAdmin, async (req, res) => {
     }
 });
 
-//if session id is priovided then it will expire provided session Otherwise current session would be logged out
+//if session id is provided then it will expire provided session Otherwise current session would be logged out
 router.put("/logout", verifyUser, async (req, res) => {
-    await prisma.userSession.update({
+    prisma.userSession.update({
         where: {
-            id: req.body.sessionId? req.body.sessionId : req.session
+            id: req.body.sessionId ? req.body.sessionId : req.session
         }, data: {
             token: null
         }
-    }).then((e) => {
-        return res.send(e);
-    })
+    }).then(() => res.send("User logged out successfully"));
 })
 
-router.get("/sessions",verifyUser,async (req, res)=>{
+//get user's active sessions
+router.get("/sessions", verifyUser, async (req, res) => {
     let sessions = await prisma.userSession.findMany({
-        where:{
-            userId : req.user.id,
+        where: {
+            userId: req.user.id,
         },
     })
     sessions = sessions.filter(s => {
