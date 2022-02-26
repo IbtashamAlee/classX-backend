b3e626c68e60603e129dc11304b013fa84bde15c
faseehahmad00 <faseehahmad00@gmail.com>
2022-02-27 01:46:54 +0500

    added accept/reject endpoint

--- routes/institute.js
+++ routes/institute.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const {PrismaClient} = require(".prisma/client");
 const prisma = new PrismaClient();
 const {verifyUser} = require('../middlewares/verifyUser');
+const {verifySystemAdmin} = require('../middlewares/verifySystemAdmin');
 
-router.get('/',async(req,res)=>{
+router.get('/',verifyUser,verifySystemAdmin,async(req,res)=>{
     const institutes = await prisma.institute.findMany();
     return res.status(200).json(institutes);
 })
 
-router.get('/requests',verifyUser,async(req,res)=>{
+router.get('/requests',verifyUser,verifySystemAdmin,async(req,res)=>{
     const requests = await prisma.instituteRequest.findMany();
     return res.status(200).json(requests);
 })
@@ -24,6 +25,29 @@ router.post('/request',verifyUser,async(req,res)=>{
     })
     return res.json(request);
 })
+
+router.put('/request/process/:id',verifyUser,verifySystemAdmin,async(req,res)=>{
+    if(!req.query.method || (req.query.method !== 'accept' && req.query.method !== 'reject'))
+        return res.status(403).send("invalid request");
+    const result  = await prisma.instituteRequest.findUnique({
+        where : {
+            id: parseInt(req.params.id)
+        }
+    });
+    if(result.acceptedAt !== null || result.rejectedAt !== null)  return res.send("request already processed");
+    const request  = await prisma.instituteRequest.update({
+        where:{
+            id : parseInt(req.params.id)
+        },
+        data:{
+            acceptedAt : req.query.method === 'accept' ? new Date() : null ,
+            rejectedAt : req.query.method === 'reject' ? new Date() : null
+        }
+    })
+    res.json(request)
+})
+
+//todo : write accept request function that includes explicit permissions for admins.
 
 
 module.exports = router;
