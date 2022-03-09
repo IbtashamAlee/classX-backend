44b935b6b20e5079e6a671f4b2cdb7d2df4a2c11
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-03-09 14:37:31 +0500

    refactoring

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,67 +1,66 @@
-const{ PrismaClient } = require('@prisma/client')
+const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
 
-async function createSystemAdminRole(){
+async function createSystemAdminRole() {
     const permissions = [
-        { name:"Can approve institute Requests", code:'01' },
-        { name:"Can read, delete an institute", code:'02' },
-        { name:"Can read, delete a department", code:'03' },
-        { name:"Can read, delete a class", code:'04' },
-        { name:"Can read, delete a user", code:'05' }
+        {name: "Can approve institute Requests", code: '01'},
+        {name: "Can read, delete an institute", code: '02'},
+        {name: "Can read, delete a department", code: '03'},
+        {name: "Can read, delete a class", code: '04'},
+        {name: "Can read, delete a user", code: '05'}
     ]
 
-        const role  = await prisma.role.create({
-            data:{
-                name: "SystemAdmin"
+    const role = await prisma.role.create({
+        data: {
+            name: "SystemAdmin"
+        }
+    })
+
+    permissions.map(async per => {
+        const permission = await prisma.permission.create({
+            data: {
+                name: per.name,
+                code: per.code
             }
         })
-
-        permissions.map(async per => {
-            const permission = await prisma.permission.create({
-                data:{
-                    name:per.name,
-                    code:per.code
-                }
-            })
-            const rolePermission = await prisma.rolePermission.create({
-                data:{
-                    permissionId: permission.id,
-                    roleId : role.id
-                }
-            })
-            console.log(rolePermission)
+        const rolePermission = await prisma.rolePermission.create({
+            data: {
+                permissionId: permission.id,
+                roleId: role.id
+            }
         })
+        console.log(rolePermission)
+    })
 
 
 }
 
-async function makeSystemAdmin(email){
-    const user  = await prisma.user.findUnique({
-        where:{
-            email:email
+async function makeSystemAdmin(email) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: email
         }
     })
-    if(user){
+    if (user) {
         const role = await prisma.role.findUnique({
-            where:{
-                name:"SystemAdmin"
+            where: {
+                name: "SystemAdmin"
             }
         });
         await prisma.userRole.create({
-            data:{
-                userId:user.id,
-                roleId:role.id
+            data: {
+                userId: user.id,
+                roleId: role.id
             }
         })
-    }
-    else
+    } else
         console.log("User not found");
 }
 
 
-createSystemAdminRole().then(()=>{
+createSystemAdminRole().then(() => {
     makeSystemAdmin("faseehahmad21@gmail.com")
-        .then(()=> {
+        .then(() => {
             console.log("user now has admin previleges");
         })
 })
