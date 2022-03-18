db24b357f33276babbf941e946fbee6acecc388e
faseehahmad00 <faseehahmad00@gmail.com>
2022-03-18 20:26:57 +0500

    safeAwait and reset password fix

--- routes/authentication.js
+++ routes/authentication.js
@@ -33,7 +33,7 @@ router.post(`/signup`, signupValidation, async (req, res) => {
         },
     }));
     if (newUserErr) return res.status(409).send("unable to create user");
-    const [_, emailErr] = await safeAwait(sendVerification(req.body.name, newUser.email, emailVerificationToken, newUser.id));
+    const [, emailErr] = await safeAwait(sendVerification(req.body.name, newUser.email, emailVerificationToken, newUser.id));
     if (emailErr) return res.status(200).send("user created successfully.unable to send email try again")
     return res.status(200).send("user created successfully");
 });
@@ -42,14 +42,14 @@ router.post(`/signup`, signupValidation, async (req, res) => {
 //This route processes user request for email verification
 router.get("/mail-verify/:token", async (req, res) => {
     const userId = req.params.token.split('=')[1];
-    const [user, userErr] = await safeAwait(prisma.user.findUnique({
+    const [user] = await safeAwait(prisma.user.findUnique({
         where: {
             id: parseInt(userId)
         },
     }));
     if (!user) return res.status(400).send("invalid verification token.")
     if (user.isVerified) return res.send("user already verified")
-    const [updatedUser, updatedUserErr] = await safeAwait(prisma.user.update({
+    const [, updatedUserErr] = await safeAwait(prisma.user.update({
         where: {
             id: parseInt(userId),
         }, data: {
@@ -92,135 +92,133 @@ router.post(`/signin`, loginValidation, async (req, res) => {
             token: sessionToken + user.id,
         }
     }));
-    if (sessionErr) return res.status(409).send("unable to login")
+    if (!session || sessionErr) return res.status(409).send("unable to login")
     return res.status(200).send({access_token: session.token});
 })
 
 // manually resend verification to registered user
 router.post("/send-mail-verification", async (req, res) => {
     const mail = req.body.email;
-    const user = await prisma.user.findUnique({
+    const [user, userErr] = await safeAwait(prisma.user.findUnique({
         where: {
             email: mail,
         },
-    });
+    }))
     if (!user) return res.status(404).send("User not Found");
+    if (userErr) return res.status(409).send("unable to find user");
     const validPassword = await verifyPassword(req.body.password, user.password)
     if (!validPassword) return res.status(401).send("Not Authorized");
     if (user.isVerified) return res.send("User is already Verified");
-    const [e, emailErr] = await safeAwait(sendVerification(user.name, user.email, user.emailToken, user.id));
-    return emailErr ? res.status(409).send("unable to send verification\n" + emailErr) : res.send("verification code sent")
+    const [, emailErr] = await safeAwait(sendVerification(user.name, user.email, user.emailToken, user.id));
+    return emailErr ? res.status(409).send({
+        message: "unable to send verification",
+        err: emailErr
+    }) : res.send("verification code sent")
 });
 
 //System admin can make another user system admin
-router.post('/makeAdmin', verifyUser, verifySystemAdmin, async (req, res) => {
-    try {
-        if (!req.body.email) return res.status(401).send("email not provided");
-        const user = await prisma.user.findUnique({
-            where: {
-                email: req.body.email,
-            }
-        })
-        if (!user) return res.status(404).send("User not found");
-        const role = await prisma.role.findMany({
-            where: {
-                name: "SystemAdmin"
-            }
-        });
-        const userRole = await prisma.userRole.create({
-            data: {
-                userId: user.id, roleId: role[0].id
-            }
-        })
-        return res.status(200).send({resp: "admin privileges created for user", userRole});
-    } catch (e) {
-        if (e.code === 'P2002') {
-            return res.status(409).send("Role Already Exists");
+router.post('/make-admin', verifyUser, verifySystemAdmin, async (req, res) => {
+    if (!req.body.email) return res.status(401).send("email not provided");
+    const [user] = await safeAwait(prisma.user.findUnique({
+        where: {
+            email: req.body.email,
         }
-        return res.status(500).send("An error occurred");
-    }
+    }))
+    if (!user) return res.status(404).send("User not found");
+    const [role] = await safeAwait(prisma.role.findUnique({
+        where: {
+            name: "SystemAdmin"
+        }
+    }));
+    if (!role) return res.status(409).send("unable to find System Admin Role")
+    const [userRole, userRoleErr] = await safeAwait(prisma.userRole.create({
+        data: {
+            userId: user.id, roleId: role.id
+        }
+    }))
+    if (!userRole || userRoleErr) return res.status(409).send({
+        message: 'unable to create user role',
+        error: userRoleErr
+    });
+    return res.status(200).send({message: "admin privileges created for user " + user.email});
 });
 
 //if session id is provided then it will expire provided session Otherwise current session would be logged out
 router.put("/logout", verifyUser, async (req, res) => {
-    prisma.userSession.update({
+    const [, loggingOutErr] = await safeAwait(prisma.userSession.update({
         where: {
             id: req.body.sessionId ? req.body.sessionId : req.session
         }, data: {
             token: null
         }
-    }).then(() => res.send("User logged out successfully"));
-})
+    }));
+    if (loggingOutErr) return res.status(409).send("unable to log out");
+    return res.send("user logged out successfully");
+});
 
 //get user's active sessions
 router.get("/sessions", verifyUser, async (req, res) => {
-    let sessions = await prisma.userSession.findMany({
+    let [sessions, sessionErr] = await safeAwait(prisma.userSession.findMany({
         where: {
             userId: req.user.id,
         },
-    })
-    sessions = sessions.filter(s => {
-        return s.token !== null
-    })
-    res.json(sessions);
+    }))
+    if(!sessions || sessionErr) return res.send("unable to fetch sessions");
+    res.send(sessions);
 })
 
 router.post('/password-reset', async (req, res) => {
-    prisma.user.findUnique({
+    if (!req.body.email) return res.send("email not provided");
+    const [user, userErr] = await safeAwait(prisma.user.findUnique({
         where: {
             email: req.body.email
         }
-    }).then(user => {
-        if (user) {
-            let resetToken = randomstring.generate(64);
-            prisma.user.update({
-                where: {
-                    email: req.body.email
-                },
-                data: {
-                    resetToken: resetToken
-                }
-            }).then((response) => {
-                if (response) {
-                    resetPassword(user.name, user.email, resetToken).then((result) => {
-                        res.status(200).send("Reset link has been sent successfully")
-                    }).catch(err => {
-                        console.log(err);
-                        res.status(409).send("Unable to send email");
-                    })
-                } else {
-                    res.status(404).send("Could not find user");
-                }
-            }).catch(err => {
-                res.status(409).send("Unable to update reset link");
-            })
+    }));
+    if (!user || userErr) return res.status(404).send("user not found");
+    const token = randomstring.generate((64));
+    const [updatedUser, updatedErr] = await safeAwait(prisma.user.update({
+        where: {
+            email: req.body.email
+        },
+        data: {
+            resetToken: token,
+            resetTokenGen: new Date()
         }
-    }).catch(err => {
-        res.status(409).send("Something went wrong");
-    })
+    }))
+    if (updatedErr) return res.status(409).send("unable to genereate reset code")
+    const [, resetMailErr] = await safeAwait(resetPassword(user.name, user.email, token, user.id));
+    if (resetMailErr) return res.status(409).send("unable to send password reset email")
+    return res.send(updatedUser);
 })
 
-router.get("/password-reset/:token", async (req, res) => {
+router.post("/password-reset/:token", async (req, res) => {
+    if (!req.body.password) return res.status(404).send("password not provided");
+    const [newPassword] = await safeAwait(encryptPassword(req.body.password))
     const temp = req.params.token.split("=");
     const token = temp[0];
-    const mail = temp[1];
-    prisma.user.findUnique({
+    const userId = temp[1];
+    const [user, userErr] = await safeAwait(prisma.user.findUnique({
         where: {
-            email: mail,
+            id: parseInt(userId),
         },
-    }).then(user => {
-        if (user) {
-            if (token !== user.resetToken) {
-                res.status(400).send("invalid reset token.")
-            } else {
-                res.status(200).send("Reset token verified!");
-            }
-        } else {
-            res.status(404).send("Unable to find user")
+    }))
+    if (!user || userErr) return res.status(404).send("unable to find user");
+    //check if code is generated within 24 hrs
+    if (!(user.resetToken === token)) return res.status(409).send("invalid token");
+    if (!(parseInt(new Date() - user.resetTokenGen) < (24 * 60 * 60 * 1000)))
+        return res.status(409).send("Password Reset Token has expired. Please try again");
+    const [updatedUser, updatedErr] = await safeAwait(prisma.user.update({
+        where: {
+            id: parseInt(userId)
+        },
+        data: {
+            password: newPassword,
+            resetToken: null,
+            resetTokenGen: new Date()
         }
-    }).catch(err => {
-        res.status(409).send("Unable to fetch user");
-    })
+    }));
+    if (!updatedUser || updatedErr) return res.status(409).send({message: "unable to reset password", err: updatedErr});
+    return res.send("password updated successfully");
 })
 
 module.exports = router;
