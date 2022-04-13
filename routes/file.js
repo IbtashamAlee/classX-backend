907d3e69f4fc252559cb75e9f110136f13b3b5a0
faseehahmad00 <faseehahmad00@gmail.com>
2022-04-13 15:45:23 +0500

    file extention and download link fixes

--- routes/file.js
+++ routes/file.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const aws = require('aws-sdk')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
+const {nanoid} = require("nanoid/async");
 
 
 const s3 = new aws.S3({
@@ -11,34 +12,40 @@ const s3 = new aws.S3({
   Bucket: process.env.AWS_BUCKET_NAME
 })
 const upload = multer({
+  limits: {fileSize: 10 * 1024 * 1024},
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_BUCKET_NAME,
+    contentType: function (req, file, cb) {
+      cb(null, file.mimetype)
+    },
+    acl: 'public-read',
+    key: async function (req, file, cb) {
+      console.log(file)
+      cb(null, file.originalname.split('.')[0] + "__" + await nanoid() + '.' + file.originalname.split('.')[1])
+    }
   })
 })
 
-router.get('/getdownloadlink', async (req, res) => {
-  if (req.body.key) {
+router.get('/download-link/:key', async (req, res) => {
     return res.send(s3.getSignedUrl('getObject', {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: req.body.key, //filename
-      Expires: 1000 //time to expire in seconds
+      Key: req.params.key, //filename
+      Expires: 120 //time to expire in seconds
     }))
-  }
-  return res.status(404).send('key not found')
 })
 
 //Uploading single File to aws s3 bucket
-router.post('/upload', upload.single('file'), function (req, res, next) {
+router.post('/', upload.single('file'), function (req, res, next) {
   if (req.file)
     return res.send(req.file);
   return res.status(404).send("file not found");
 })
 
 //Uploading Multiple Files to aws s3 bucket
-router.post('/uploadmultiple', upload.array('file', 5), function (req, res, next) {
+router.post('/multiple', upload.array('file', 5), function (req, res, next) {
   if (req.files) {
-    res.send({
+    return res.send({
       data: req.files,
       msg: 'Successfully uploaded ' + req.files.length + ' files!'
     })
