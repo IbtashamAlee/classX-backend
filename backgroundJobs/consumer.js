4581ee9de566f1518552769951d5cb5b07f8aeef
faseehahmad00 <faseehahmad00@gmail.com>
2022-05-26 16:42:43 +0500

    fixed background operations

--- backgroundJobs/consumer.js
+++ backgroundJobs/consumer.js
@@ -2,15 +2,15 @@ const Bull = require("bull");
 const EmailService = require('./email-service');
 const sendVerification = EmailService.sendVerification;
 
-const consumer = function() {
-  console.log("hello")
-  const signupVerificationQueue = new Bull('signup-verification', 'bullx');
-  signupVerificationQueue.process(async function(job) {
-    const {data} = job
+const consumer = function () {
+  console.log("Bull Started!");
+  const signupVerificationQueue = new Bull('signup-verification');
+  signupVerificationQueue.process(async function (job) {
+    let data = job['data'];
     console.log(data)
-    await sendVerification(data.name, data.mail, data.token, data.userId)
-      .then(()=>console.log("verification email sent successfully"))
-      .catch((e)=>console.log(e))
+    sendVerification(data.name, data.mail, data.token, data.userId)
+      .then(() => console.log("verification email sent successfully"))
+      .catch((e) => console.log(e))
     return {email: data.email, result: "OK"};
   });
 };
